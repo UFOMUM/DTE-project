304145a2645d1a74adf397ec39f28b15f77ef67f
UFOMUM <xavcla32@kingshigh.school.nz>
2026-05-12 11:01:48 +1200

    Safe Space

---
+++ Kill_Zone_(Real).gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var death_timer: Timer = $"Death Timer"
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("IDK")
+	death_timer.start()
+
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://Game_Over_(Real).tscn")
--- Text_Fake_Death.gd
+++ Text_Fake_Death.gd
@@ -8,4 +8,4 @@ extends Label
 
 
 func _on_area_2d_mouse_shape_exited(shape_idx: int) -> void:
-	get_tree().change_scene_to_file("res://max_level.tscn")
+	get_tree().change_scene_to_file("res://Game_Over_(Real).tscn")
