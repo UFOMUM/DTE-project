36871ce0ac01ec84c84d0e590842f2fb9ba81775
UFOMUM <xavcla32@kingshigh.school.nz>
2026-05-27 12:25:42 +1200

    giod good

---
+++ Scripts/Restart.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			get_tree().change_scene_to_file("res://Start_Menu.tscn")
